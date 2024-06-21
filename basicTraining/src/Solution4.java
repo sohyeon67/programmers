778ad096e5ed451c810d32baeac8730ae24178eb
sohyeon67 <sohyunj67@gmail.com>
2024-06-21 17:59:28 +0900

    수 조작하기1

--- basicTraining/src/Solution4.java
+++ basicTraining/src/Solution4.java
@@ -1,6 +1,7 @@
 public class Solution4 {
     public int solution(int n, String control) {
         int answer = n;
+
         char c = '\u0000';
         for(int i=0; i<control.length(); i++) {
             c = control.charAt(i);
@@ -9,6 +10,17 @@ public class Solution4 {
             if (c == 'd') answer += 10;
             if (c == 'a') answer -= 10;
         }
+
+//        for(char ch : control.toCharArray()) {
+//            switch(ch) {
+//                case 'w': answer += 1; break;
+//                case 's': answer -= 1; break;
+//                case 'd': answer += 10; break;
+//                case 'a': answer -= 10; break;
+//                default:break;
+//            }
+//        }
+
         return answer;
     }
 }
