8ee0d4e618f560018e04a09aa58dbca8a47eeb48
sohyeon67 <sohyunj67@gmail.com>
2024-06-21 16:15:23 +0900

    문자열 뒤의 n글자

--- basicTraining/src/Solution1.java
+++ basicTraining/src/Solution1.java
@@ -1,2 +1,8 @@
 public class Solution1 {
+    public String solution(String my_string, int n) {
+        String answer = "";
+        int index = my_string.length() - n;
+        answer = my_string.substring(index);
+        return answer;
+    }
 }
